e3bcd701a45f5220d4a9a9b0a8aec632885ae76d
unknown <ebaadali@gmail.com>
2019-09-17 01:45:07 -0400

    Fixed Links, and Updated Resume

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -12,7 +12,7 @@ export default class Porfolio extends Component {
             resumeData.portfolio && resumeData.portfolio.map((item)=>{
               return(
                 <div className="columns portfolio-item">
-                <a href="https://www.google.ca" target="_blank" rel="noopener noreferrer">
+                <a href={item.projurl} target="_blank" rel="noopener noreferrer">
                   <div className="item-wrap">
                       <img src={`${item.imgurl}`} alt={item.description} className="item-img"/>
                       <div className="overlay">
