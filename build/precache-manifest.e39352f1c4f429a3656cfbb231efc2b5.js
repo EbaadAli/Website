d90257862f35be478fd7b3b30aa7ca152b44c891
Ebaad Ali <ebaadali@gmail.com>
2025-01-21 03:26:44 -0500

    build upate

---
+++ build/precache-manifest.e39352f1c4f429a3656cfbb231efc2b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6eb256e0c6ed7dc28801",
+    "url": "/static/css/main.fb24f065.chunk.css"
+  },
+  {
+    "revision": "6eb256e0c6ed7dc28801",
+    "url": "/static/js/main.6eb256e0.chunk.js"
+  },
+  {
+    "revision": "d494210123104ea4c6b2",
+    "url": "/static/js/1.d4942101.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9d0eadc894164270e2bd28d7a293ac4f",
+    "url": "/index.html"
+  }
+];
